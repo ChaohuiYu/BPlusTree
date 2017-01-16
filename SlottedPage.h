49d5b6740f5c23c4d90b76d6646b96b3339afafe
R04942031 <R04942031@ntu.edu.tw>
2017-01-16 21:49:26 +0800

    Update SlottedPage.h

--- SlottedPage.h
+++ SlottedPage.h
@@ -5,20 +5,20 @@
 #include <vector>
 using namespace std;
 
-class Relation{
+class SlottedPage{
 	public:
-		Relation(string name,string datatype,int reclen);
+		SlottedPage(string name,string datatype,int reclen);
 		void insert(int key, string record); //回傳RID
 		void delete(int key); //回傳Delelte 那筆record 的RID
-
+		bool checkFun();//check RIDtable中前面的值是否都填滿
 
 	private:
-	  int slot_num;
-	  int pageID_base;
-	  map<int, string> key2record;
-	  map<int, int> key2RID;
-	  vector<int> delete_list;
-	  int currenRID=0;
-	  string R_name;
-	  string R_datatype;
+		int slot_num;
+		int pageID_base;
+		map<int, string> key2record;
+		map<int, int> key2RID;
+		map<int, bool> RIDtable;
+		int currenRID=0;
+		string R_name;
+		string R_datatype;
 };
