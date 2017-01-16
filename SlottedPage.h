b2468f66af947b84baacecfa9bfd47b0bc973cfc
ChaohuiYu <r05942046@ntu.edu.tw>
2017-01-16 20:41:19 +0800

    Update SlottedPage.h
    function declaration

--- SlottedPage.h
+++ SlottedPage.h
@@ -8,8 +8,8 @@ using namespace std;
 class Relation{
 	public:
 		Relation(string name,string datatype,int reclen);
-	
-	
+		void insert(int key, string record); //回傳RID		
+		void delete(int key); //回傳Delelte 那筆record 的RID	
 	
 	
 	private:
