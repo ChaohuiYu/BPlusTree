4e1ff4f0116c975f351ce4cc35051293a8a33db6
R04942031 <R04942031@ntu.edu.tw>
2017-01-17 15:23:18 +0800

    Update SlottedPage.h

--- SlottedPage.h
+++ SlottedPage.h
@@ -11,6 +11,7 @@ class SlottedPage{
 		void insert(int key, string record); //回傳RID
 		void delete(int key); //回傳Delelte 那筆record 的RID
 		bool checkFun();//check RIDtable中前面的值是否都填滿
+		int getnextRID(int RID);
 
 	private:
 		int slot_num;
@@ -18,7 +19,8 @@ class SlottedPage{
 		map<int, string> key2record;
 		map<int, int> key2RID;
 		map<int, bool> RIDtable;
-		int currenRID=0;
+		int currentRID=0; //掃把指標
+		int updatedRID=0; //指到目前未被分配出去的最小RID值，如已分配完0~4頁，則updatedRID值為500
 		string R_name;
 		string R_datatype;
 };
