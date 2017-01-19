657be6432a2f1f0e22a186522b867c26a55435c1
Roger-Wu <gsetcrw@gmail.com>
2017-01-20 02:34:55 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,13 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <set>
-#include <tuple>
-#include <ctime>
 #include "parser.h"
 using namespace std;
 
 int main() {
     const string inputData = "data/input_data.txt";
-    const string testData = "data/ProjectB_data.txt";
 
-    parseCommands(testData);
+    parseCommands(inputData);
 
     return 0;
 }
